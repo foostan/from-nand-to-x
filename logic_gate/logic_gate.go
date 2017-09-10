1acb0472eb1728f9449a79371ff0b250a971204e
foostan <ks@fstn.jp>
2017-09-11 02:03:07 +0900

    Fix indent

--- logic_gate/logic_gate.go
+++ logic_gate/logic_gate.go
@@ -1,9 +1,9 @@
 package logic_gate
 
 type SingleInputLogicGate interface {
-  Calc(a bool) bool
+	Calc(a bool) bool
 }
 
 type DoubleInputLogicGate interface {
-  Calc(a bool, b bool) bool
+	Calc(a bool, b bool) bool
 }
