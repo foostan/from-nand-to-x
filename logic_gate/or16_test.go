25e6b832aa14f4dd728ce8a549ffbf8c453ffe83
foostan <ks@fstn.jp>
2017-09-13 20:40:57 +0900

    Add and16 logic gate

--- logic_gate/or16_test.go
+++ logic_gate/or16_test.go
@@ -15,11 +15,6 @@ func TestAnd16Calc(t *testing.T) {
 func test_calc_for_and16(t *testing.T, lg Double16InputLogicGate) {
 	test_input_calc_for_double_16_input(t, lg, []ExpectForDouble16Input{
 		{
-			A:   [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			B:   [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			OUT: [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-		},
-		{
 			A:   [16]bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
 			B:   [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
 			OUT: [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
