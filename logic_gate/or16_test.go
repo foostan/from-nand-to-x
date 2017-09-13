cbae0bfba4e265f139a1b96c9f1356ca28171653
foostan <ks@fstn.jp>
2017-09-13 20:41:56 +0900

    Add or16 logic gate

--- logic_gate/or16_test.go
+++ logic_gate/or16_test.go
@@ -4,25 +4,30 @@ import (
 	"testing"
 )
 
-func TestBasicAnd16Calc(t *testing.T) {
-	test_calc_for_and16(t, BasicAnd16{})
+func TestBasicOr16Calc(t *testing.T) {
+	test_calc_for_or16(t, BasicOr16{})
 }
 
-func TestAnd16Calc(t *testing.T) {
-	test_calc_for_and16(t, And16{})
+func TestOr16Calc(t *testing.T) {
+	test_calc_for_or16(t, Or16{})
 }
 
-func test_calc_for_and16(t *testing.T, lg Double16InputLogicGate) {
+func test_calc_for_or16(t *testing.T, lg Double16InputLogicGate) {
 	test_input_calc_for_double_16_input(t, lg, []ExpectForDouble16Input{
 		{
-			A:   [16]bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
+			A:   [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
 			B:   [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
 			OUT: [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
 		},
 		{
+			A:   [16]bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
+			B:   [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+			OUT: [16]bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
+		},
+		{
 			A:   [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
 			B:   [16]bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
-			OUT: [16]bool{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+			OUT: [16]bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
 		},
 		{
 			A:   [16]bool{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
